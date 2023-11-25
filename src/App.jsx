311bf99d0b498d3c29c681cf94f69463e0aa6bd6
miracle ezeh <amichy07@gmail.com>
2023-11-25 06:56:24 +0100

    #info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
+import { netflix } from "./constants/data";
+import Container from "./components/Container";
 
 export default function App() {
   return (
     <div>
       <Header />
-      
+      <section
+        style={{
+          
+          width: "80%",
+          margin: "0 auto",
+        }}
+      >
+        {netflix.map((data) => {
+          if (data.id === 2 || data.id === 4) {
+            return (
+              <Container key={data.id} orderOne={1} orderTwo={2} {...data} />
+            );
+          } else {
+            return <Container key={data.id} {...data} />;
+          }
+        })}
+      </section>
     </div>
-  )
+  );
 }
