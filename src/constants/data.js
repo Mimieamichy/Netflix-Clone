dcb77e48b1568b5ea9220aa5ee29c38e1432a337
miracle ezeh <amichy07@gmail.com>
2023-11-25 06:44:25 +0100

    data info

---
+++ src/constants/data.js
@@ -0,0 +1,104 @@
+import imageOne from "../imgs/tv.png"
+import imageTwo from "../imgs/mobile.jpg"
+import imageFour from "../imgs/kids.png"
+export const netflix = [
+  {id:1,
+    title: "Enjoy on your TV",
+    info: " Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+    img: imageOne,
+    alt: "tv"
+},
+  {id:2,
+    title: "Download your shows to watch offline",
+    info: "Save your favorites easily and always have something to watch.",
+    img: imageTwo,
+    alt: "mobile"
+},
+  {id:3,
+    title: "Watch everywhere",
+    info: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+    
+},
+  {id:4,
+    title: "Create profiles for kids",
+    info: "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
+    img: imageFour,
+    alt: "kids"
+},
+]
+
+export const accodian =[
+  {
+    id:1,
+    plus: "+",
+    minus: "--",
+    title: "What is Netflix?",
+    text: "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.You can watch as much as you want, whenever you want without a single commercial all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!"
+  },
+  {
+    id:2,
+    plus: "+",
+    minus: "--",
+    title: "How much does Netflix cost?",
+    text: "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₦1,200 to ₦4,400 a month. No extra costs, no contracts."
+  },
+  {
+    id:3,
+    plus: "+",
+    minus: "--",
+    title: "Where can i watch?",
+    text: "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere."
+  },
+  {
+    id:4,
+    plus: "+",
+    minus: "--",
+    title: "How do i cancel?",
+    text: "Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees - start or stop your account anytime."
+  },
+  {
+    id:5,
+    plus: "+",
+    minus: "--",
+    title: "What can i watch on Netflix?",
+    text: "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want."
+  },
+  {
+    id:6,
+    plus: "+",
+    minus: "--",
+    title: "Is Netflix good for kids?",
+    text: "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don't want kids to see."
+  }
+]
+
+export const footerList = [
+  {
+    id:1,
+    l1: "FAQ",
+    l2: "Investor Relations",
+    l3: "Privacy",
+    l4: "Speed Test"
+  },
+  {
+    id:2,
+    l1: "Help Center",
+    l2: "Jobs",
+    l3: "Cookie Prefrences",
+    l4: "Legal Notices"
+  },
+  {
+    id:3,
+    l1: "Account",
+    l2: "Ways to Watch",
+    l3: "Corporate Information",
+    l4: "Only On Netflix"
+  },
+  {
+    id:4,
+    l1: "Media Center",
+    l2: "Terms of Use",
+    l3: "Contact Us",
+    
+  }
+]
